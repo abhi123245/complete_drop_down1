02e92b4801e4523e2c56000a4fa9273e9eccabe1
unknown <you@example.com>
2019-03-13 09:45:32 +0530

    first commit

---
+++ api/findcustomer.js
@@ -0,0 +1,31 @@
+const express = require("express");
+var ex = express();
+const Form1= require("../schema/Form");
+const bodyparser = require("body-Parser");
+
+var router = express.Router();
+router.use(bodyparser.json())
+router.route("/")
+.get(async (req,res,next)=>
+{
+    console.log("wellcome");
+  
+})
+.post(async (req,res,next)=>
+{  id=req.body.value;
+  pagenumber=req.body.pagenumber;
+  size=req.body.size;
+ await Form1.customer1.find({agent_id:id},function(err,docs){
+
+ })
+  
+    
+    .then((data)=>
+    {
+console.log(data)
+res.status(200).json(data);
+
+  })
+
+})
+module.exports=router;
